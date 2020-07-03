8bc85c23e3b12682c452303c2f46e28f571073a4
Christopher Singley <csingley@gmail.com>
2020-07-03 12:00:30 -0500

    Empty out sonarr.models.system, which is angering mypy

--- downloadcarr/sonarr/models/system.py
+++ downloadcarr/sonarr/models/system.py
@@ -6,38 +6,3 @@ https://github.com/Sonarr/Sonarr/wiki/System-Backup
 
 DiskSpace, RootFolder, SystemStatus SystemBackup implemented in downloadcarr.models.common
 """
-from dataclasses import dataclass
-from typing import Tuple, Optional
-from datetime import datetime
-
-#  from downloadcarr.models import Base, DiskSpace, SystemStatus
-
-
-#  @dataclass(frozen=True)
-#  class UnmappedFolder(Base):
-#      """Attribute of RootFolder"""
-
-#      name: str
-#      path: str
-
-
-#  @dataclass(frozen=True)
-#  class RootFolder(Base):
-#      """Returned by /rootfolder."""
-
-#      path: str
-#      freeSpace: int
-#      unmappedFolders: Tuple[UnmappedFolder, ...]
-#      id: int
-#      totalSpace: Optional[int] = None
-
-
-#  @dataclass(frozen=True)
-#  class SystemBackup(Base):
-#      """Returned by /system/backup."""
-
-#      name: str
-#      path: str
-#      type: str
-#      time: datetime
-#      id: int
